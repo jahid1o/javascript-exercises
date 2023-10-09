4ffe8f41b963669c10afc00f67bbd815445c88e9
jahid <jahid1o@outlook.com>
2023-10-09 19:08:40 +0600

    Completed excercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = (word, count) => {
+    if (Math.sign(count) == -1){
+        return 'ERROR'
+    }
+    let repeatedString = ""
 
+    for (let x = 0; x < count; x++){
+        repeatedString += word
+    }
+
+    return repeatedString
 };
 
 // Do not edit below this line
