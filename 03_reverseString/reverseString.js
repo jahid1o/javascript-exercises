3b21fea629a063753b8c72197382d2bc9f53d8f9
jahid <jahid1o@outlook.com>
2023-10-09 21:05:03 +0600

    Completed excercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringReversed = ""
 
+    for (let i = 1, j = string.length; i <= j; i++) {
+        stringReversed += string[j - i]
+    }
+    return stringReversed
 };
 
 // Do not edit below this line
