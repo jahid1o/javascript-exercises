354aac58a52eb481ac30cebeb14e9cf4791dcae1
jahid <jahid1o@outlook.com>
2023-10-10 23:13:51 +0600

    Completed excercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    for (let i = 1, j = arguments.length; i < j; i++) {
+        for (let k = 0, l = arguments[0].length; k < l; k++) {
+            if (arguments[i] === arguments[0][k]) {
+                arguments[0].splice(k, 1)
+            }
+        }
+    }
+    return arguments[0]
 };
 
 // Do not edit below this line
