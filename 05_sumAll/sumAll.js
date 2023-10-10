bee730a26e35e30f76c91598936c29d33111bd0b
jahid <jahid1o@outlook.com>
2023-10-10 23:27:29 +0600

    Completed excercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = (from, to) => {
+    if (from > to) {
+        [from, to] = [to, from]
+    }
+    if (typeof from != "number" || typeof to != "number" || Math.sign(from) == -1 || Math.sign(to) == -1) {
+        return 'ERROR'
+    }
+    let sum = 0
+    for (let i = from; i <= to; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
