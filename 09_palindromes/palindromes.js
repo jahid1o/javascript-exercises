04a83d65ee51ef500ed62678cf108ffd0ff75b3f
jahid <40070056+jahid1o@users.noreply.github.com>
2023-10-31 14:22:21 +0600

    Completed exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let arr = [...text]
+        .map(l => l.toLowerCase())
+        .filter(l => /^[A-Z0-9]$/i.test(l)
+    )
 
+    const textLength = arr.length
+    for (let i = 0; i < textLength; i++) {
+        if (arr[i] != arr[(textLength - 1) - i]) {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
