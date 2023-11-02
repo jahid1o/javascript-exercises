0fcdfe6a2abac8344ce64891c507b5fb5e517832
jahid <40070056+jahid1o@users.noreply.github.com>
2023-11-02 17:50:23 +0600

    Completed exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = people => {
+    people = people
+        .map(person => {
+            let p = person
+            if (!p.yearOfDeath) {
+                p.yearOfDeath = (new Date()).getFullYear()
+            }
+            return p
+        })
+        .sort((a, b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1)
 
+    return people[0]
 };
 
 // Do not edit below this line
