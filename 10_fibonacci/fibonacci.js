5073d415354cc5484b85c25a8f2af4960974f7af
jahid <40070056+jahid1o@users.noreply.github.com>
2023-11-02 16:09:47 +0600

    Completed exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = member => {
+    member = Number(member)
+    if (typeof member != 'number' || member < 0) {
+        return "OOPS"
+    }
+    let f1 = 1, f2 = 1, currentMember = 1
 
+    while (currentMember < member) {
+        let f = f1 + f2
+        f1 = f2
+        f2 = f
+        currentMember++
+    }
+    return f1
 };
 
 // Do not edit below this line
